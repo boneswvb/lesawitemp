1706322c915e5ac896315d136fbb06526a5765f8
Wim von Benecke <boneswvb@gmail.com>
2022-11-26 09:57:14 +0200

    First load 26-11-2022

---
+++ index.php
@@ -0,0 +1,55 @@
+<?php
+
+$errors = [];
+$errorMessage = '';
+
+if (!empty($_POST)) {
+   $name = $_POST['name'];
+   $phone = $_POST['phone'];
+   $email = $_POST['email'];
+   $message = $_POST['message'];
+
+   if (empty($name)) {
+       $errors[] = 'Name is empty';
+   }
+
+   if (empty($email)) {
+       $errors[] = 'Email is empty';
+   } else if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+       $errors[] = 'Email is invalid';
+   }
+
+   if (empty($message)) {
+       $errors[] = 'Message is empty';
+   }
+
+   if (empty($errors)) {
+       $toEmail = "info@lesawi.co.za";
+       $emailSubject = 'New email from your contact form - Lesawi.co.za';
+       $headers = ['From' => $email, 'Reply-To' => $email, 'Content-type' => 'text/html; charset=utf-8'];
+       $bodyParagraphs = ["Name: {$name}", "Phone {$phone}", "Email: {$email}", "Message:", $message];
+       $body = join(PHP_EOL, $bodyParagraphs);
+
+       if (mail($toEmail, $emailSubject, $body, $headers)) 
+           header('Location: sucesspage.html');
+       } else {
+           $errorMessage = 'Oops, something went wrong. Please try again later';
+       }
+
+   } else {
+
+       $allErrors = join('<br/>', $errors);
+       $errorMessage = "<p style='color: red;'>{$allErrors}</p>";
+   }
+
+
+// $to_email = "info@lesawi.co.za";
+// $subject = "Simple Email Test via PHP";
+// $body = "Hi,nn This is test email send by PHP Script";
+// $headers = "From: boneswvb@gmail.com";
+ 
+// if (mail($to_email, $subject, $body, $headers)) {
+//     echo "Email successfully sent to $to_email...";
+// } else {
+//     echo "Email sending failed...";
+// }
